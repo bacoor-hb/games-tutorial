c97d8b607a288ace7441b5b7b645c51ec6155a91
truongvihuy <truongvihuy@gmail.com>
2022-03-23 14:52:34 +0700

    fix ture base

--- Assets/Scripts/Controllers/TurnBaseController.cs
+++ Assets/Scripts/Controllers/TurnBaseController.cs
@@ -5,173 +5,118 @@ using UnityEngine;
 
 public class TurnBaseController : MonoBehaviour
 {
-    [SerializeField]
-#pragma warning disable CS0436 // Type conflicts with imported type
-    public List<PlayersController> playerList;
-#pragma warning restore CS0436 // Type conflicts with imported type
-    public bool startGamge = false;
-    public int turnBase = 0;
+    public delegate void Event<T>(T data);
+    public static Event<int> OnStartTurn;
+    public static Event<int> OnEndTurn;
+    public static Event<List<int>> OnAction;
+    public static Event<int> OnActionStart;
+    public static Event<int> OnActionEnd;
+    public static Event<int> OnChangePlayer;
+
+    public static bool startGamge = false;
+    public static List<int> playerList;
+    private static int turnBase = 0;
+    private static int status = 0;
 
 
-    public void StartGame()
+    public static void StartGame()
     {
         if (!startGamge)
         {
             startGamge = true;
             turnBase = 0;
-            AddListenPlayer(turnBase);
         }
     }
 
-    public void EndGame()
+    public static void EndGame()
     {
         startGamge = false;
-        RemoveListenPlayer(turnBase);
+    }
+
+    public static int Register()
+    {
+        if (startGamge)
+        {
+            throw new System.Exception("Game start");
+        }
+        playerList.Add(playerList.Count);
+        return playerList.Count - 1;
     }
 
     private void Start()
     {
         startGamge = false;
-        //StartGame();
+        playerList = new List<int>();
     }
 
     private void Update()
     {
         if (startGamge)
         {
+            switch(status)
+            {
+                case 1:
+                    StartTurn();
+                    break;
+                case 2:
+                    // handle action
+                    break;
+                case 3:
+                    EndTurn();
+                    if (true)
+                    {
+                        ChangePlayer();
+                    }
+                    break;
+            }
 
         }
     }
 
-
-    private void NextTurn()
+    private static void StartTurn()
     {
-        StartCoroutine(OnNextTurn());
-    }
-
-    private void StartTurn()
-    {
-        StartCoroutine(OnStartTurn());
-    }
-
-    private void EndTurn()
-    {
-        StartCoroutine(OnEndTurn());
-    }
-
-    private void Action()
-    {
-        StartCoroutine(OnAction());
-    }
-
-    private void ActionStart()
-    {
-        StartCoroutine(OnActionStart());
-    }
-
-    private void ActionEnd()
-    {
-        StartCoroutine(OnActionEnd());
-    }
-
-
-    float delayTime = 2.0f;
-    IEnumerator OnNextTurn()
-    {
-        if (startGamge)
+        if (OnStartTurn != null)
         {
-            yield return new WaitForSeconds(delayTime);
-
-            Debug.Log("OnNextTurn");
-            int nextIndex = GetNextIndexPlayer(turnBase);
-            RemoveListenPlayer(turnBase);
-            AddListenPlayer(nextIndex);
-            turnBase = nextIndex;
+            OnStartTurn(turnBase);
         }
+        status++;
     }
 
-    IEnumerator OnStartTurn()
+    private static void EndTurn()
     {
-        if (startGamge)
+        if (OnEndTurn != null)
         {
-            yield return new WaitForSeconds(delayTime);
-
-            Debug.Log("OnStartTurn");
-
+            OnEndTurn(turnBase);
         }
+        status = 1;
     }
 
-    IEnumerator OnEndTurn()
+    private static void ChangePlayer()
     {
-        if (startGamge)
+        if (OnChangePlayer != null)
         {
-            yield return new WaitForSeconds(delayTime);
-
-            Debug.Log("OnEndTurn");
+            OnChangePlayer(NextPlayer());
         }
     }
 
-    IEnumerator OnAction()
+    private static int NextPlayer()
     {
-        if (startGamge)
+        return (turnBase + 1) % playerList.Count;
+    }
+
+    private static void ActionStart()
+    {
+        if (OnActionStart != null)
         {
-            yield return new WaitForSeconds(delayTime);
-
-            Debug.Log("OnEndTurn");
+            OnActionStart(turnBase);
         }
     }
 
-    IEnumerator OnActionStart()
+    private static void ActionEnd()
     {
-        if (startGamge)
+        if (OnActionEnd != null)
         {
-            yield return new WaitForSeconds(delayTime);
-
-            Debug.Log("OnActionStart");
+            OnActionEnd(turnBase);
         }
-    }
-
-    IEnumerator OnActionEnd()
-    {
-        if (startGamge)
-        {
-            yield return new WaitForSeconds(delayTime);
-
-            Debug.Log("OnActionEnd");
-
-        }
-    }
-
-    private void AddListenPlayer(int indexPlayer)
-    {
-        if (playerList[indexPlayer])
-        {
-            playerList[indexPlayer]?.OnStartTurn();
-            playerList[indexPlayer].OnStartTurn += StartTurn;
-            playerList[indexPlayer].OnEndTurn += EndTurn;
-            playerList[indexPlayer].OnAction += Action;
-            playerList[indexPlayer].OnActionStart += ActionStart;
-            playerList[indexPlayer].OnActionEnd += ActionEnd;
-            playerList[indexPlayer].OnChangePlayer += NextTurn;
-        }
-    }
-
-    private void RemoveListenPlayer(int indexPlayer)
-    {
-        if (playerList[indexPlayer])
-        {
-            playerList[indexPlayer].OnStartTurn -= StartTurn;
-            playerList[indexPlayer].OnEndTurn -= EndTurn;
-            playerList[indexPlayer].OnAction -= Action;
-            playerList[indexPlayer].OnActionStart -= ActionStart;
-            playerList[indexPlayer].OnActionEnd -= ActionEnd;
-            playerList[indexPlayer].OnChangePlayer -= NextTurn;
-        }
-    }
-
-    private int GetNextIndexPlayer(int indexPlayer)
-    {
-        int nextIndex = (indexPlayer + 1) % playerList.Count;
-        return nextIndex;
     }
 }
