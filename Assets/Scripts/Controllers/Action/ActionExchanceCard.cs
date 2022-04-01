5f3249597c7d8fa0638a4c29dad22e42e7da4a4d
truongvihuy <truongvihuy@gmail.com>
2022-04-01 09:55:32 +0700

    action exchance card

---
+++ Assets/Scripts/Controllers/Action/ActionExchanceCard.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionExchanceCard : IAction
+{
+    public ACTION GetAction()
+    {
+        return ACTION.RUN_THE_CELL;
+    }
+    public void OnStartAction()
+    {
+
+    }
+    public void OnAction()
+    {
+
+    }
+    public void OnEndAction()
+    {
+
+    }
+}
