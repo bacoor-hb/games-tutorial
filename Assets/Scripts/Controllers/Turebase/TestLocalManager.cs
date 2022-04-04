258ada9e93b2e4749728b0f8118356b273fc5b6d
truongvihuy <truongvihuy@gmail.com>
2022-04-04 14:35:20 +0700

    turnbase

---
+++ Assets/Scripts/Controllers/Turebase/TestLocalManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TestLocalManager : MonoBehaviour
+{
+  TurnBaseController turnBaseController = new TurnBaseController();
+
+  void Start()
+    {
+      
+    }
+}
