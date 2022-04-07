b3cdf4f248129e1931eb2238f404fdf4db900ddf
truongvihuy <truongvihuy@gmail.com>
2022-04-07 10:35:25 +0700

    chance card manager

--- Assets/Scripts/Controllers/ChanceCardManager/ChanceCardManager.cs
+++ Assets/Scripts/Controllers/ChanceCardManager/ChanceCardManager.cs
@@ -2,23 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChanceCardManager : MonoBehaviour
+public class ChanceCardManager
 {
     [SerializeField]
     private List<ChanceCard> chanceCards;
     [SerializeField]
     private List<ChanceCard> luckyCards;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public ChanceCard GetRandomChanceCard()
     {
