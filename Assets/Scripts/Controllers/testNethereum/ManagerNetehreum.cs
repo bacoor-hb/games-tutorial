5e2b20f6279a39f11948dc87414c90f7c8bd22ad
Hungduc123 <ducpham6966@gmail.com>
2022-04-28 11:03:30 +0700

    update get balance

--- Assets/Scripts/Controllers/testNethereum/ManagerNetehreum.cs
+++ Assets/Scripts/Controllers/testNethereum/ManagerNetehreum.cs
@@ -1,3 +1,6 @@
+using Nethereum.JsonRpc.UnityClient;
+using Nethereum.RPC.Eth.DTOs;
+using Nethereum.Util;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +10,20 @@ public class ManagerNetehreum : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
+        StartCoroutine(loadBalance());
     }
 
     // Update is called once per frame
-    void Update()
+    IEnumerator loadBalance()
     {
-        
+
+
+        var balanceRequest = new EthGetBalanceUnityRequest("https://kovan.infura.io/v3/e4c6b2743e544bdb910ef53155687b0f");
+        yield return balanceRequest.SendRequest("0xC0b4ec83028307053Fbe8d00ba4372384fe4b52B", BlockParameter.CreateLatest());
+
+        var BalanceAddressTo = UnitConversion.Convert.FromWei(balanceRequest.Result.Value);
+
+
+        Debug.Log("Balance of account:" + BalanceAddressTo);
     }
 }
